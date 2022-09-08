7616ab5c8aeda207907f1dcbd4c95147a5d016f1
kawawamarc <marcillineous@gmail.com>
2022-09-08 15:15:11 -0700

    prints a string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- * main-Entry point
- * Return: 0 if successful
- */
-int main(void) 
+ *  * main - main block
+ *   * Return: 0
+ *    */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
-
