b881a8f62828eab8614ecc8bd4c6e39d8521bc0c
kawawamarc <marcillineous@gmail.com>
2022-09-08 15:28:33 -0700

    Print some string'

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+ * main - main block
+ * Return: 0 if succesful
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
