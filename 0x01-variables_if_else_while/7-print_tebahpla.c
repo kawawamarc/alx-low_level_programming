2e42afd1949b9dbfb94923ef84fa946162c78f6c
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-09 16:48:16 -0700

    Print alphabets in reverse order

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
- *main ->Print alphabets in the reverse order
- *use putchar
- *Return: Always 0 (Success)
- */
+*main ->Print alphabets in the reverse order
+*use putchar
+*Return: Always 0 (Success)
+*/
 int main(void)
 {
  int ch;
  for (ch = 122; ch >= 97; ch--)
-{
+ {
 	 putchar(ch);
  }
  putchar(10);
