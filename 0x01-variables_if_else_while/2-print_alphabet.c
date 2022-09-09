80dbf5d5bcba4af0c64bae34c5e6eeb0df39aba8
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-09 13:54:02 -0700

    Print lowercase alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main -> assign a random number to the variable n each time it is executed
+ *print alphabets in lowercase
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	putchar(10); 
+
+	return (0);
+}
