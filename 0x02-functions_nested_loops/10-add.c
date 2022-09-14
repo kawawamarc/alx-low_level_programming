2e2197b07dd8031477e8a627128090f219277a15
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-13 19:39:00 -0700

    A function that adds two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add -> A function that adds two integers and prints result
+* @x : param1
+* @y : param2
+* Return: x + y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
+
