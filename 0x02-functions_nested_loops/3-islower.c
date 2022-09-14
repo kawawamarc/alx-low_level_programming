ecea69bf7f359f82ae98c11705c684c34e92cb3a
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-13 18:38:36 -0700

    A function that returns 1 or 0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _islower -> A function that checks for lowercase character
+* @c: a charcter argument
+* Return: Returns 1 if parameter is lowercase and 0 if otherwise
+*/
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
