50bb8d4c768c9c5778388dd6c86e4c4a74d3995b
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-15 10:23:47 -0700

    Printing number values within range

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* mul -> A function that multiplies two integer values
+* @a: First argument or parameter as int
+* @b: Second argument or parameter as int
+* Return: Success if 0
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
