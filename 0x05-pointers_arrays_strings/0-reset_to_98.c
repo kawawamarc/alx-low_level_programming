3a6118ec3263f65b45e266079edd0514c819e529
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-19 12:18:41 -0700

    Using pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - a function that takes a pointer to anint parameter
+ *@n: A pointer to an int that will be updated
+ *
+ *Return: void that means is correct
+ */
+
+void reset_to_98(int *n)
+
+{
+*n = 98;
+}
