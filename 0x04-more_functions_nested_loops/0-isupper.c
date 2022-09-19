86b6d5008a379c50650c13268f6b3c9160418299
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-19 13:21:50 -0700

    Function to print square numbers

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,22 +1,20 @@
 #include "main.h"
-
 /**
-* _isupper -> A function to check if a character is upper case 
-* @c: an argument/parameter of type int
-* Return: Success if 0
-*/
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
 
 int _isupper(int c)
 {
 if (c >= 'A' && c <= 'Z')
 {
-	return (1);
+return (1);
 }
 
 else
 {
-	return (0);
+return (0);
 }
 
 }
-
