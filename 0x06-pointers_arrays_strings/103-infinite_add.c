debeaa9f74dfa113d1bcad85705ef9ad4e9c31e7
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-22 10:55:01 -0700

    A function that adds two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,12 +1,12 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * infinite_add - A function that adds two numbers
- * @n1: First number .
- * @n2: Second number .
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
  * @r: buffer that the function will use to store the result.
  * @size_r: buffer size:
- * Return: return the stored value in the buffer function.
+ * Return: the pointer to dest.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
