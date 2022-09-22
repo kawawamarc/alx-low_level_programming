2e1d3bbe388d9eaff17c54875709fbae0b88bd25
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-22 03:10:51 -0700

    A function that prints an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - A function that prints an integer.
+ * @n: input integer.
+ * Return: A void function.
+ */
+void print_number(int n)
+{
+	unsigned int i, j, count;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = n * -1;
+	}
+	else
+	{
+		i = n;
+	}
+
+	j = i;
+	count = 1;
+
+	while (j > 9)
+	{
+		j /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((i / count) % 10) + 48);
+	}
+}
