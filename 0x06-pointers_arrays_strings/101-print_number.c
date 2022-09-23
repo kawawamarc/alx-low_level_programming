efcf086db4e8a0eec2441cf0b798a35952685378
kawawamarc <65120942+kawawamarc@users.noreply.github.com>
2022-09-22 17:13:44 -0700

    A function that prints an integer

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  *print_number - A function that prints an integer
@@ -8,19 +9,40 @@
 
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	int i, l, x, h, ans, consN, superN;
 
 	if (n < 0)
-	{
 		_putchar('-');
-		n1 = -n;
-	}
 
-	if (n1 / 10 != 0)
+	consN = n, ans = n, superN = n;
+
+	if (n < 0)
+		for (l = 0; superN < 0; ++l)
+			superN /= 10;
+	else
+		for (l = 0; superN > 0; ++l)
+			superN /= 10;
+
+	h = l - 1;
+
+	if (n == 0)
 	{
-		print_number(n1 / 10);
+		_putchar('0');
 	}
-	_putchar((n1 % 10) + '0');
+	else
+	{
+		for (i = 0; i < l; i++)
+		{
+			x = h;
+			while (x)
+			{
+				ans = ans / 10;
+				x--;
+			}
+			ans =  ans % 10;
+			_putchar(abs(ans)  + '0');
+			ans = consN;
+			h--;
+		}
+	}
 }
